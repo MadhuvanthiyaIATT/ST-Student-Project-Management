3f79211edb678e5ccf7f1da188c0a356ce7afca7
Shravan7 <devibhavani845@gmail.com>
2026-05-27 17:25:39 +0530

    student login

--- src/test/java/gitcommands/Firstcommit.java
+++ src/test/java/gitcommands/Firstcommit.java
@@ -1,5 +1,6 @@
 package gitcommands;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -7,6 +8,8 @@ public class Firstcommit {
 	public static void main(String[] args) {
 		WebDriver driver= new ChromeDriver();
 		driver.manage().window().maximize();
+	driver.get("https://www.instagram.com/accounts/login/?hl=en");
+	driver.findElement(By.xpath("//input[@type='text']")).sendKeys("tom___murphy__");
 	}
 
 }
