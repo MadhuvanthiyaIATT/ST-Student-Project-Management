e727f6091f62af730af58f905ac9a89538f5b0d3
Mohnish <mohnishmohnish69@gmail.com>
2026-05-27 18:08:03 +0530

    newChanges

--- src/test/java/gitcommands/Firstcommit.java
+++ src/test/java/gitcommands/Firstcommit.java
@@ -1,5 +1,6 @@
 package gitcommands;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -7,6 +8,10 @@ public class Firstcommit {
 	public static void main(String[] args) {
 		WebDriver driver= new ChromeDriver();
 		driver.manage().window().maximize();
+		
+	driver.get("https://www.facebook.com/login.php/");
+	driver.findElement(By.xpath("//input[@autocomplete='username webauthn']")).sendKeys("jonny1234@gmail.com");
+	
 	}
 
 }
